0364b06a18396c702e0104da8e4a29eb1f0c3a17
Jon Mooney <jon.mooney@yahoo.com>
2022-03-16 11:09:10 -0700

    added inquirer main menu, got view departments to work

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+const cTable = require('console.table');
 require('dotenv').config();
 
 // Connect to database
@@ -13,9 +14,90 @@ const db = mysql.createConnection(
   console.log(`Connected to the employee_tracker_db database.`)
 );
 
-db.query(`SELECT * FROM roles`, (err, result) => {
-    if (err) {
-      console.log(err);
+let finished = false;
+
+const main = async () => {
+    // Main loop until user selects 'Exit Application'
+    while(!finished){
+        clearConsole();
+        
+        const {choice} = await promptMain();
+
+        if(choice === 'Exit Application'){
+            finished = true;
+        }else if(choice === 'View all Departments'){
+            viewDepartments();
+        }else if(choice === 'View all Roles'){
+            
+        }else if(choice === 'View all Employees'){
+            //await promptEngineer();
+        }else if(choice === 'Add a Department'){
+            //await promptEngineer();
+        }
+        else if(choice === 'Add a Role'){
+            //await promptEngineer();
+        }
+        else if(choice === 'Add an Employee'){
+            //await promptEngineer();
+        }
+        else if(choice === 'Update an Employee Role'){
+            //await promptEngineer();
+        }
     }
-    console.log(result);
-});
+
+    console.log ('Bye!');
+    process.exit()
+}
+
+// Main questions for loop
+const promptMain = async () => {
+    return inquirer.prompt([
+     {
+         type: 'list',
+         name: 'choice',
+         message: '\n\nWhat would you like to do?\n',
+         choices: ['View all Departments', 'View all Roles', 'View all Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role', 'Exit Application'],
+     }
+     ])
+}
+
+const viewDepartments = () => {
+    db.query(`SELECT * FROM department`, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+
+        clearConsole();
+
+        console.log('[Departments]\n');
+        console.table(result);
+        console.log('(Press up or down to show the menu)');
+    });
+}
+
+function clearConsole() {
+    process.stdout.write("\u001b[3J\u001b[2J\u001b[1J");console.clear();
+}
+
+console.reset = function () {
+    return process.stdout.write('\033c\033[3J');
+  }
+
+main();
+
+
+// Standard query
+// db.query(`SELECT * FROM roles`, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// Prepared statement example with variable
+// db.query(`DELETE FROM books WHERE id = ?`, deletedRow, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+//   });
